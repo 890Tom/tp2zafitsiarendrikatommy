00d0a15bece1c6a543b909505a4feb42882b8f85
60 - Tommy ZAFITSIARENDRIKA <zafi.tommy@gmail.com>
2024-04-06 09:44:55 +0300

    Configuration JSF

---
+++ src/main/java/mg/zafitsiarendrika/tp2zafitsiarendrikatommy/config/ConfigJSF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.zafitsiarendrika.tp2zafitsiarendrikatommy.config;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+/**
+ *
+ * @author kk
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
